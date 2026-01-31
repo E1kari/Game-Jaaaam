e2e6bb39007dd529d6363899a0a79a0684362bd1
E1kari <josefine.cholle@STUD.HOCHSCHULE-HEIDELBERG.DE>
2026-01-31 01:36:29 +0100

    programming stuff

---
+++ Game Jaaam poggieees/Assets/GameState.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+
+    private Customer customer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
